b2274ef8041818cecde8d05624934e7cb7d4990a
joe-doe <joe.doe@engineer.com>
2016-09-06 15:55:21 +0300

    better store failure handling

--- routes/store.js
+++ routes/store.js
@@ -3,28 +3,44 @@ var router = express.Router();
 var jsdom = require('jsdom');
 
 var site = 'http://www.piraeusbank.gr/el/Idiwtes/Epitokia-Deltia-Timon/Imerisio-Deltio-Timon-Synallagmatos-Trapezogrammation';
+var db = null;
+var req = null;
+var res = null;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  var db = req.db;
-  store(db, res);
+router.get('/', function(request, result, next) {
+  db = request.db;
+  req = request;
+  res = result;
+  store();
 });
 
+function failed(e) {
+  res.json({'status': e.error});
+}
 
-function store(db, res){
+function succeeded() {
+  res.json({'status': "OK"});
+}
+
+function store(){
   jsdom.env({
     url: site,
     scripts: ["http://code.jquery.com/jquery.js"],
     done: function (err, windowq){
-          //Use jQuery just as in a regular HTML page
-          var buy = windowq.$(".PinakasSynallagmatos tr:nth-child(23) .secondcell_1").text();
-          var sell = windowq.$(".PinakasSynallagmatos tr:nth-child(23) .forthcell_1").text();
+          try {
+            //Use jQuery just as in a regular HTML page
+            var buy = windowq.$(".PinakasSynallagmatos tr:nth-child(23) .secondcell_1").text();
+            var sell = windowq.$(".PinakasSynallagmatos tr:nth-child(23) .forthcell_1").text();
 
-          var collection = db.get(req.config.get('collection_usd'));
-          collection.insert({'date': new Date(), 'buy': parseFloat(buy), 'sell': parseFloat(sell)});
-          res.json({'status': 'OK'});
-        }
-    });
+            var collection = db.get(req.config.get('collection_usd'));
+            collection.insert({'date': new Date(), 'buy': parseFloat(buy), 'sell': parseFloat(sell)});
+            succeeded();
+          } catch (e) {
+            failed(e);
+          }
+    }
+  });
 }
 
 module.exports = router;
