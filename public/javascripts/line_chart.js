8447a244e943a1285050302bba5407d2fc4f9cd8
joe-doe <joe.doe@engineer.com>
2016-09-16 15:23:33 +0300

    add sell prices on chart

--- public/javascripts/line_chart.js
+++ public/javascripts/line_chart.js
@@ -6,7 +6,7 @@ $(function(){
         labels: [],
         datasets: [
             {
-                label: "usd / euro",
+                label: "buy",
                 fill: false,
                 lineTension: 0.1,
                 backgroundColor: "rgba(75,192,192,0.4)",
@@ -26,7 +26,30 @@ $(function(){
                 pointHitRadius: 10,
                 data: [],
                 spanGaps: false,
+            },
+            {
+                label: "sell",
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: "rgba(175,92,192,0.4)",
+                borderColor: "rgba(175,92,192,1)",
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: "rgba(175,92,192,1)",
+                pointBackgroundColor: "#fff",
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: "rgba(175,92,192,1)",
+                pointHoverBorderColor: "rgba(220,220,220,1)",
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: [],
+                spanGaps: false,
             }
+
         ]
     };
 
@@ -35,6 +58,7 @@ $(function(){
         for (var i=0; i<res.length; i++) {
             data.labels.push(res[i].date.substring(0,10));
             data.datasets[0].data.push(res[i].buy);
+            data.datasets[1].data.push(res[i].sell);
         }
 
         var myLineChart = new Chart(ctx, {
